c2555c0aaf5d1c451e24063a2deb9019b2674816
Kottalo <kottaloflotenspieler@gmail.com>
2020-08-04 20:09:04 +0800

    Checkpoint

--- resources/views/manage/supplier_record.blade.php
+++ resources/views/manage/supplier_record.blade.php
@@ -1,11 +1,4 @@
 @extends('main')
 
 @section('content')
-  <div id="app">
-    <project-material></project-material>
-  </div>
-
-  <script src="{{ asset('js/vue.js') }}">
-
-  </script>
 @endsection
